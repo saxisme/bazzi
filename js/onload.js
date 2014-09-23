5bfa52ae93992bf487a7b0f0ae6fb79647ddded1
Sacha Benda <sacha.benda@gmail.com>
2014-09-22 21:22:41 -0500

    Img optimisation, slant, CF/ button

--- js/onload.js
+++ js/onload.js
@@ -2,6 +2,6 @@ jQuery(document).ready(function($) {
 	
     //position the slant div before and after - where is needed depending on .slant class
     $('.slant').prepend('<div class="tm-slant-block-bottom widget-slant"></div>');
-    $('.slant').append('<div class="tm-slant-block-top  widget-slant"></div>');
+    $('.slant').append('<div class="tm-slant-block-top widget-slant"></div>');
 
 });
