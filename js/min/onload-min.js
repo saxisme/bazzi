47511b263eaba3e2ad6aa29e56906a1a72f17c04
Sacha Benda <sacha.benda@gmail.com>
2014-09-22 19:07:58 -0500

    Slant no conflit - Footer image - Slant

--- js/min/onload-min.js
+++ js/min/onload-min.js
@@ -1,1 +1,1 @@
-jQuery(document).ready(function(){jQuery(".slant").prepend('<div class="tm-slant-block-bottom widget-slant"></div>'),jQuery(".slant").append('<div class="tm-slant-block-top  widget-slant"></div>')})();
+jQuery(document).ready(function($){$(".slant").prepend('<div class="tm-slant-block-bottom widget-slant"></div>'),$(".slant").append('<div class="tm-slant-block-top  widget-slant"></div>')});
--- js/onload.js
+++ js/onload.js
@@ -1,7 +1,7 @@
-jQuery(document).ready(function() {
+jQuery(document).ready(function($) {
 	
     //position the slant div before and after - where is needed depending on .slant class
-    jQuery('.slant').prepend('<div class="tm-slant-block-bottom widget-slant"></div>');
-    jQuery('.slant').append('<div class="tm-slant-block-top  widget-slant"></div>');
+    $('.slant').prepend('<div class="tm-slant-block-bottom widget-slant"></div>');
+    $('.slant').append('<div class="tm-slant-block-top  widget-slant"></div>');
 
-})();
+});
